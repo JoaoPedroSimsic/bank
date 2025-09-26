0d84a45bea154a873a697c7a8ec5be9db3ddd150
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2025-09-26 15:46:52 -0300

    fix: adding user checking in transactions.h

--- include/transactions.h
+++ include/transactions.h
@@ -5,6 +5,6 @@
 
 int deposit (sqlite3 *db, int account_id, double amount);
 int withdraw(sqlite3 *db, int account_id, double amount);
-int transfer (sqlite3 *db, int from_id, double amount);
+int transfer (sqlite3 *db, int user_id, int from_id, int to_id, double amount);
 
 #endif 
